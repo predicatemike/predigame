ed27ea2c670a0e1c1ff57c0db72cd2bdcd8e299e
Gabe-Linux <gabecicione@gmail.com>
2017-04-11 13:16:06 -0400

    Rename Sprite method "move_keys" to "keys"; Add "bouncy" and "bounce" methods.

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ SIZE = 100 # optional grid square size in pixels
 grid() # draw the coordinate grid
 
 shape().float().speed(1) # a simple and likely shape use case
-img().move_keys() # a simple and likely image use case
+img().keys() # a simple and likely image use case
 
-shape(RECT, CYAN, (6, 6), (2, 2), outline = 10).move_keys('d', 'a', 'w', 's').speed(10) # create a shape using all possible parameters
+shape(RECT, CYAN, (6, 6), (2, 2), outline = 10).keys('d', 'a', 'w', 's').speed(10) # create a shape using all possible parameters
--- predigame/Sprite.py
+++ predigame/Sprite.py
@@ -1,19 +1,60 @@
-import random
+import sys, random, pygame
 from functools import partial
 from .utils import *
 from . import globs
 
 class Sprite:
     def __init__(self, surface, rect):
+        if len(globs.sprites) >= 9000:
+            sys.exit('Too many sprites! You\'re trying to spawn over 9,000!')
         self.surface = surface.convert_alpha()
         self.origin_surface = self.surface
         self.rect = rect
         self.move_speed = 5
-        self.move_vec = [self.rect.x, self.rect.y] # destination coordinates
+        self.vel = (0, 0)
+        self.move_vec = [self.rect.x, self.rect.y]
         self.float_vec = [0, 0, 0] # x distance, y distance, offset distance
         self.collisions = []
 
     def _update(self):
+        x_vel, y_vel = self.vel
+        if x_vel and not abs(x_vel) == self.move_speed:
+            x_vel = self.move_speed * (x_vel / abs(x_vel))
+        if y_vel and not abs(y_vel) == self.move_speed:
+            y_vel = self.move_speed * (y_vel / abs(y_vel))
+
+        self.vel = (x_vel, y_vel)
+
+        self.move_vec[0] += self.vel[0]
+        self.move_vec[1] += self.vel[1]
+
+        self._update_bounce()
+
+        self._update_float()
+
+        self._update_move()
+
+        self._handle_collisions()
+
+    def _draw(self, surface):
+        surface.blit(self.surface, self.rect)
+
+    def _handle_collisions(self):
+        for collision in self.collisions:
+            if not collision['sprite'] in globs.sprites:
+                self.collisions.remove(collision)
+                continue
+
+            if self.rect.colliderect(collision['sprite'].rect):
+                collision['cb'](self, collision['sprite'])
+                break # only handle one collision per frame (for now)
+
+    def _is_moving(self):
+        if self.rect.x == self.move_vec[0] and self.rect.y == self.move_vec[1]:
+            return False
+        return True
+
+    def _update_move(self):
         if not self.rect.x == self.move_vec[0]:
             dist = self.move_vec[0] - self.rect.x
             if abs(dist) < self.move_speed:
@@ -30,6 +71,18 @@ class Sprite:
                 move = (dist) / abs(dist) * self.move_speed
             self.rect.y += move
 
+    def _update_bounce(self):
+        if self.rect.x + self.rect.width > globs.WIDTH and self.vel[0] > 0:
+            self.bounce(True, False)
+        elif self.rect.x < 0 and self.vel[0] < 0:
+            self.bounce(True, False)
+
+        if self.rect.y + self.rect.height > globs.HEIGHT and self.vel[1] > 0:
+            self.bounce(False, True)
+        elif self.rect.y < 0 and self.vel[1] < 0:
+            self.bounce(False, True)
+
+    def _update_float(self):
         float_dist = self.float_vec[2]
         if float_dist and not self._is_moving():
             if not self.rect.x % float(globs.GRID_SIZE):
@@ -45,25 +98,6 @@ class Sprite:
             self.move_vec[0] += self.float_vec[0]
             self.move_vec[1] += self.float_vec[1]
 
-        self._handle_collisions()
-
-    def _draw(self, surface):
-        surface.blit(self.surface, self.rect)
-
-    def _handle_collisions(self):
-        for collision in self.collisions:
-            if not collision['sprite'] in globs.sprites:
-                self.collisions.remove(collision)
-                continue
-
-            if self.rect.colliderect(collision['sprite'].rect):
-                collision['cb'](self, collision['sprite'])
-
-    def _is_moving(self):
-        if self.rect.x == self.move_vec[0] and self.rect.y == self.move_vec[1]:
-            return False
-        return True
-
     def _move(self, direction, distance = 1):
         if self._is_moving():
             return
@@ -77,7 +111,7 @@ class Sprite:
         elif direction == 'down' and not self.rect.y + roundup(self.rect.height, globs.GRID_SIZE) == globs.HEIGHT:
             self.move_vec[1] += globs.GRID_SIZE * distance
 
-    def move_keys(self, right = 'right', left = 'left', up = 'up', down = 'down'):
+    def keys(self, right = 'right', left = 'left', up = 'up', down = 'down'):
         if right:
             register_keydown(right, partial(self._move, 'right'))
         if left:
@@ -95,12 +129,20 @@ class Sprite:
         return self
 
     def float(self, distance = 0.25):
+        if float(distance).is_integer():
+            distance += 0.1
         self.float_vec[2] = int(globs.GRID_SIZE * distance)
 
         return self
 
-    def when_collides(self, sprite, callback):
-        self.collisions.append({ 'sprite': sprite, 'cb': callback })
+    def collides(self, sprites, callback):
+        if not isinstance(sprites, list):
+            sprites = [sprites]
+
+        for sprite in sprites:
+            if sprite == self:
+                continue
+            self.collisions.append({ 'sprite': sprite, 'cb': callback })
 
         return self
 
@@ -110,6 +152,25 @@ class Sprite:
         self.rect.width = width
         self.rect.height = height
         self.surface = pygame.transform.smoothscale(self.origin_surface, self.rect.size).convert_alpha()
+
+        return self
+
+    def bounce(self, bounce_x = True, bounce_y = True):
+        x_vel, y_vel = self.vel
+        if bounce_x:
+            x_vel = -x_vel
+        if bounce_y:
+            y_vel = -y_vel
+
+        self.vel = x_vel, y_vel
+
+        return self
+
+    def bouncy(self):
+        if not self._is_moving():
+            x_vel = random.randrange(-self.move_speed, self.move_speed + 1, self.move_speed * 2)
+            y_vel = random.randrange(-self.move_speed, self.move_speed + 1, self.move_speed * 2)
+            self.vel = x_vel, y_vel
 
         return self
 
