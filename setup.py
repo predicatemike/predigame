976bfcb9245ac8a601263c9d34a774aaf1406910
Gabe-Linux <gabecicione@gmail.com>
2017-06-05 12:01:08 -0400

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'predigame',
-    version = '0.3.2',
+    version = '0.4.0',
     description = 'A Python based game development platform',
     url = 'http://predicate.us',
     author = 'Predicate Academy',
