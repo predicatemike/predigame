2dd86c0b5179363a5c8a810b893bffdfbbd8e209
morr <morr@twosixlabs.com>
2018-01-11 07:45:42 -0500

    sprite example

--- examples/sprites/sprite-demo.py
+++ examples/sprites/sprite-demo.py
@@ -2,15 +2,53 @@ WIDTH = 30
 HEIGHT = 20
 TITLE = 'Sprites Demo'
 
-# create sprites and position them
-# shapes and images
 
-# show effects all in a single window
+text("Shape Sprites", BLACK, pos = (0,0), size = 1.25)
 
-# document the process
+# create a circle
+shape(CIRCLE, RED, (2, 2))
 
-def on_click(s):
-	image('redsplat', s.event_pos, 1).fade(15)
+# create a big blue circle next to the red one
+shape(CIRCLE, BLUE, (5, 1), 3)
 
-p1 = image('kenny', (15,10), 10)
-p1.clicked(on_click)
+# create a 2x2 ORANGE square at position (8,2)
+shape(RECT, ORANGE, (10, 2), (2,2))
+
+# create a 6x1 rectangle
+shape(RECT, AQUA, (15, 2), (6, 1))
+
+# create a custom colored (r, g, b) ELLIPSE
+shape(ELLIPSE, (134, 134, 134), (23, 2), (5, 2))
+
+text("Image Sprites", BLACK, pos = (0, 6), size = 1.25)
+
+image('sprite', (2, 8))
+image('coke', (7, 8), size = 2)
+image('zombie-1', (13,9), size = 5)
+image('zombie-2', (19,9), size = 5)
+
+text("Sprite Effects", BLACK, pos = (0, 12), size = 1.25)
+
+image('sprite', (2,14)).spin(time=1)
+image('sprite', (7,14)).pulse(time=0.5, size=3)
+image('coke', (13, 15), size = 2).speed(1).float(distance=1)
+
+def doit(s):
+	s.destroy()
+	image('kaboom', (15, 10), size=25)
+
+image('clickme', (19, 15), size = 2).pulse(time=0.05, size= 1.25).clicked(doit)
+
+
+s = image('coke', (13, 15), size = 2)
+s.speed(1).bouncy().spin().pulse().clicked(s.destroy)
+
+def eatit(z, s):
+	s.destroy()
+	z.scale(1.2)
+
+
+image('zombie-1', (28, 18), size = 2).keys().collides(sprites(), eatit)
+image('zombie-2', (25, 18), size = 2).keys(right='d', left='a', up='w', down='s').collides(sprites(), eatit)
+keydown('r', reset)
+
--- predigame/api.py
+++ predigame/api.py
@@ -4,5 +4,5 @@ from random import choice, shuffle
 from .predigame import actor, image, shape, sound, text, grid, time, callback, score, destroyall, pause, resume, reset, quit, screenshot
 from .constants import *
 from .utils import register_keydown as keydown, at, get
-from .utils import animate, rand_pos, rand_arc, distance, visible
+from .utils import animate, rand_pos, rand_arc, distance, visible, sprites
 from .Thing import Thing
