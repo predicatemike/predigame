523fb43981d8c4f30f848d013cf9eb293f8899a7
Mike Orr <mike@predicate.us>
2018-01-27 16:47:24 -0500

    doc updates

--- examples/actors/actor-test.py
+++ examples/actors/actor-test.py
@@ -1,9 +1,14 @@
 WIDTH = 30
 HEIGHT = 18
-TITLE = 'MAZE'
+TITLE = 'Making Bacon'
+
+# use a grass background
 BACKGROUND = 'grass'
+
+# how many piggies to create
 PIGGIES = 10
 
+# create a daedalus maze with stone images
 maze(callback=partial(image, 'stone'))
 
 # a callback that keeps the player from running
@@ -15,9 +20,14 @@ def evaluate(action, sprite, pos):
     else:
         return True
 
-# create a soldier actor with keyboard movements 
-# these movements that can be aborted (if other movements are made)
-player = actor('Soldier-2', (0, HEIGHT-1), tag='player', abortable=True).keys(precondition=evaluate)
+# create a soldier on the bottom left grid cell
+player = actor('Soldier-2', (0, HEIGHT-1), tag='player', abortable=True)
+
+# have the solider attach to the keyboard arrows
+# each move is "evaluated" to make sure the player
+# doesn't walk through the wall
+player.keys(precondition=evaluate)
+
 # player moves at a speed of 5 with an animation rate of 2
 # which flips the sprite image every other frame
 player.speed(5).rate(2).move_to((0, HEIGHT-1))
@@ -28,13 +38,13 @@ def create_piggy(num):
 		pos = rand_pos()
 		piggy = actor('Piggy', pos, tag='piggy')
 		piggy.move_to((pos))
-		# graze is a random walking
+		# graze is a random walk
 		piggy.wander(graze, time=0.4)
 
 # create some piggies
 create_piggy(PIGGIES)
 
-# it's an air shot
+# shoot a weapon
 def shoot():
 	player.act(SHOOT, loop=1)
 
@@ -54,11 +64,13 @@ def shoot():
 	if score() == PIGGIES:
 		text('Time for some BACON!! (%s secs)' % time(), color=BLACK)
 
+# register space to shoot
+keydown('space', shoot)
+
 #we're keeping score
 score()
 
 # register the 'r' key for resetting the game
 keydown('r', reset)
 
-# register space to shoot
-keydown('space', shoot)
+
