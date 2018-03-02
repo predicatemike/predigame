0caaf51ee76e23706845db3f3c6160f588fa34b8
Mike Orr <mike@predicate.us>
2018-03-01 20:09:04 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'predigame',
-    version = '0.8.10',
+    version = '0.8.11',
     description = 'A Python based game development platform',
     url = 'http://predicate.us',
     author = 'Predicate Academy',
