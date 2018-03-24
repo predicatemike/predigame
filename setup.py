4cc65682360c2f4ca200c416f8cde1509a202584
Mike Orr <mike@predicate.us>
2018-03-24 13:45:51 -0400

    final updates

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'predigame',
-    version = '1.0.1',
+    version = '1.1.0',
     description = 'A Python based game development platform',
-    url = 'http://predicate.us',
+    url = 'http://predigame.io',
     author = 'Predicate Academy',
-    author_email = 'info@predicate.us',
+    author_email = 'support@predigame.io',
     packages = find_packages(),
     install_requires = ['pygame', 'pillow', 'astar'],
     include_package_data = True,
     entry_points = {
         'console_scripts': [
-            'pigm = predigame:main'
+            'pred = predigame:main'
         ]
     }
 )
